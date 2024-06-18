fe156357c8956c8c74b64e1b9d95cfb7f7e2434c
kkumyk <k.kumykova@gmail.com>
2024-06-18 20:26:06 +0100

    update fizzbuzz solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -20,20 +20,20 @@ def fizzBuzz(n):
 print(fizzBuzz(15))
 
 
-# def fizzBuzz(n):
-#     result = []
+def fizzBuzz(n):
+    result = []
     
-#     for i in range(1, n+1):
-#         if i % 3 == 0 and i % 5 == 0:
-#             result.append("FizzBuzz")
-#         elif i % 3 == 0:
-#             result.append("Fizz")
-#         elif i % 5 == 0:
-#             result.append("Buzz")
-#         else:
-#             result.append(str(i))
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
     
-#     return result
+    return result
 
 # Further reading: https://www.enjoyalgorithms.com/blog/fizz-buzz-problem
 
