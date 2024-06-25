01c9c4e20b46479c18b8246399ff9eaab8e746af
kkumyk <k.kumykova@gmail.com>
2024-06-25 15:49:26 +0100

    product of array except self solution

---
+++ LeetCode/product_of_array_except_self.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    
+    def productExceptSelf(self, nums):
+        
+        res = [1] * (len(nums)) # [1, 1, 1, 1]
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i] # [1, 1, 2, 6]
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res # [1, 1, 2, 6]
+
+
+print(Solution.productExceptSelf(object, [1,2,3,4]))
