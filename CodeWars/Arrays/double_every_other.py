d05e7ca269c7b0082cd86d91a83e9c2bc048f2a0
kkumyk <k.kumykova@gmail.com>
2024-06-26 11:43:01 +0100

    double every other list comprehensions solution

--- CodeWars/Arrays/double_every_other.py
+++ CodeWars/Arrays/double_every_other.py
@@ -11,7 +11,11 @@ def double_every_other(lst):
             res.append(v*2)
         else:
             res.append(v)
-    return res    
+    return res
+
+
+def double_every_other(lst):
+    return [x * 2 if x % 2 else x for i, x in enumerate(lst)]
 
 
 print(double_every_other([1,2,3,4,5])) # [1,4,3,8,5]
