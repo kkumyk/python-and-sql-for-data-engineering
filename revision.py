bc537fbce4e7b7a88de3428a0d415d103e7cf7ec
kkumyk <k.kumykova@gmail.com>
2024-07-02 12:51:03 +0100

    add revised solutions

--- revision.py
+++ revision.py
@@ -1,3 +1,21 @@
+'''
+write a function to calculate the sum of the numerical values in a nested list:
+sum_nested([1, [2, [3, [4]]]]) -> 10
+'''
+
+# def sum_nested(lst):
+#     sum = 0
+    
+#     for el in lst:
+#         if isinstance(el, list):
+#             sum += sum_nested(el)
+#         else:
+#             sum += el
+#     return sum
+        
+
+# print(sum_nested([1, [2, [3, [4]]]]))
+
 
 '''
 - Write a function that takes in an array of integers from 0-9, and returns a new array:
@@ -13,66 +31,45 @@ Rules and assertions
 '''
 # [0, 4, 6, 8, 8, 8, 5, 5, 7] =>  [1, 1, 1, 3, 2, 1] => [3, 1, 1, 1] => [1, 3] => [1, 1] => [2]
 
-def set_reducer(array):
-    res = []
-    ind = 0 # a counter to go over the initial array
+# def set_reducer(array):
+#     res = []
+#     ind = 0 # a counter to go over the initial array
     
-    while ind < len(array):
-        val_counter = 1 # counter to track how many times the value is repeated
+#     while ind < len(array):
+#         counter = 1
         
-        while ind+1 < len(array) and array[ind] == array[ind+1]:
-            val_counter += 1
-            ind += 1
-        res.append(val_counter if val_counter > 1 else 1)
-        ind += 1
+#         while ind+1 < len(array) and array[ind] == array[ind+1]:
+#             counter += 1
+#             ind += 1
+#         res.append(counter if counter > 1 else 1)
+#         ind += 1
+#     if len(res) == 1:
+#         return res[0]
+#     else:
+#         return set_reducer(res)
+        
+# print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7]))
+
+
+# def set_reducer(array):
+#     res = []
+#     ind = 0 # a counter to go over the initial array
+    
+#     while ind < len(array):
+#         val_counter = 1 # counter to track how many times the value is repeated
+        
+#         while ind+1 < len(array) and array[ind] == array[ind+1]:
+#             val_counter += 1
+#             ind += 1
+#         res.append(val_counter if val_counter > 1 else 1)
+#         ind += 1
             
-    if len(res) == 1:
-        return res[0]
-    else:
-        return set_reducer(res)
+#     if len(res) == 1:
+#         return res[0]
+#     else:
+#         return set_reducer(res)
 
-print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7])) # 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7]))
 
 
 '''
