2db6c439f3620641f71c54ecc151c7b889817353
kkumyk <k.kumykova@gmail.com>
2024-08-13 13:22:20 +0100

    add comments and annotation to anagrams from hash solution

--- Recursion/anagrams_from_hash.py
+++ Recursion/anagrams_from_hash.py
@@ -1,13 +1,15 @@
-def get_words(hash_of_letters):
-    
-    def get_values(dic):
-        array_of_stings = ["".join(v*k) for v,k in dic.items()]
-        letters = "".join(map(str, array_of_stings))
-        return letters
+# https://www.codewars.com/kata/543e926d38603441590021dd
 
+def get_words(hash_of_letters):
+    # combine all elements from the values array into a single string:
+    def get_values(dic: dict) -> str:
+        array_of_stings = ["".join(v*k) for v,k in dic.items()] # ['aa', 'bc']
+        letters = "".join(map(str, array_of_stings)) # aabc
+        return letters
     all_letters = get_values(hash_of_letters)
     
-    def rec(string):
+    # return all variation of the letter combinations found in all_letters using recursion:
+    def rec(string: str) -> list:
         base = 1
         if len(string) <= base:
             return string
@@ -16,9 +18,10 @@ def get_words(hash_of_letters):
             for var in rec(string[1:]):
                 for i in range(len(string)):
                     vars.append(var[:i] + string[0:1] + var[i:])
+                    
             return sorted(set(vars))
     
-    return rec(all_letters)
-    
+    return rec(all_letters) # ['aabc', 'aacb', 'abac', 'abca', 'acab', 'acba', 'baac', 'baca', 'bcaa', 'caab', 'caba', 'cbaa']
 
-print(get_words({2:["a"], 1:["b", "c"]}))
+
+print(get_words({2:["a"], 1:["b", "c"]})) 
