e326a85b3276ac9162186bf35b7fb5cc08bae76b
kkumyk <k.kumykova@gmail.com>
2024-08-14 18:05:47 +0100

    add select in solution

---
+++ SQL-Basics/select-basics.sql
@@ -0,0 +1,8 @@
+/*
+Select basics exercises from sqlzoo.net
+*/
+
+/* Use IN to check if an item is in the list*/
+SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway')
+
+/* Next: https://sqlzoo.net/wiki/SELECT_Quiz */
