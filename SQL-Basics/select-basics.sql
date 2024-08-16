19350d1e7dfc83210a21595f68db8eea081eee28
kkumyk <k.kumykova@gmail.com>
2024-08-16 13:15:07 +0100

    add sql solutions with select

--- SQL-Basics/select-basics.sql
+++ SQL-Basics/select-basics.sql
@@ -1,8 +1,25 @@
-/*
-Select basics exercises from sqlzoo.net
-*/
+/*SELECT basics exercises from https://sqlzoo.net/wiki/SQL_Tutorial */
 
 /* Use IN to check if an item is in the list*/
 SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway')
 
-/* Next: https://sqlzoo.net/wiki/SELECT_Quiz */
+/*
+LIKE determines whether a specific character string matches a patter: regex or wildcards
+% - any string of zero or more characters
+Select all countries and their populations starting with "Al":
+*/
+SELECT name, population FROM world WHERE name LIKE "Al%"
+
+/*Show the countries which have a name that includes the word 'United': */
+select name from world where name like "%United%"
+
+/*
+Show the name and population in millions for the countries of the continent 'South America'.
+Divide the population by 1000000 to get population in millions.
+*/
+select name, population/1000000, continent from world where continent="South America"
+
+
+
+
+
