cc14041fcac285444c98eb01a785b7a393d9eea1
kkumyk <k.kumykova@gmail.com>
2024-08-17 20:12:34 +0100

    add 11/57 sql solution with focus on converting one data type to another using CAST in Postgres

---
+++ 57-SQL-Practice-Problems/011.sql
@@ -0,0 +1,24 @@
+/* MS SQL Server */
+
+SELECT
+    FirstName
+    , LastName
+    , Title
+    , DataOnlyBirthData = convert(date, BirthDate)
+FROM Employees
+Order By BirthDate;
+
+/* PostgreSQL
+
+CAST function converts one data type to another.
+CAST syntax: CAST(expression AS target_data_type) with expression to be value or column to be converted. 
+Use DATE keyword to convert the column to the DATE date type which only stores the date part (year, month, day).
+*/
+
+SELECT
+    "FirstName",
+    "LastName",
+    "Title",
+    CAST("BirthDate" AS DATE) AS "DataOnlyBirthData"
+FROM "Employees"
+ORDER BY "BirthDate";
