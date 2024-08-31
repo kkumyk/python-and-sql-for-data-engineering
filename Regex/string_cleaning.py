d4729a8ae942c722a871b45e718db296196660dc
kkumyk <k.kumykova@gmail.com>
2024-08-31 16:28:28 +0100

    add second regex solution to the sting cleaning function with comments on used method

--- Regex/string_cleaning.py
+++ Regex/string_cleaning.py
@@ -26,6 +26,27 @@ def string_clean(s):
     return ''.join(re.findall('\D', s))
 
 
+'''
+Raw String Literal
+- "r" is used as a prefix to indicate that the given string is a raw string literal in Python;
+- with the "r" prefix Python will treat backslashes "\" as a literal character and not as escape character;
+- without using "r", you will need double backslashes for the "\d" (so "\\d" instead of "\d") to be interpreted correctly;
+
+\d
+- matches any Unicode decimal digit including [0-9]
+- opposite of \D which matches any character which is not a decimal digit;
+
+re.sub(pattern, replacement*, string)
+- * pattern can be a string of a function
+- returns the string by replacing the left most occurrences of pattern in string by the provided replacement;
+
+The function below searches the provided string for all occurrences of digits and replaces them with an empty string;
+'''
+
+def string_clean(s):
+    return re.sub(r'\d','',s)
+
+
 print(string_clean('This looks5 grea8t!'))
 print(string_clean('! !'))
 print(string_clean('12345*6789'))
