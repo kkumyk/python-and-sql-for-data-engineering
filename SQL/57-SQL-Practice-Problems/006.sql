9f05ccde654cd7a0d96e3305b0bf5bb8a79fc828
kkumyk <k.kumykova@gmail.com>
2024-09-03 12:37:05 +0100

    update supplies and contacttitles solution

---
+++ SQL/57-SQL-Practice-Problems/006.sql
@@ -0,0 +1,21 @@
+select
+    SupplierID,
+    ContactName,
+    ContactTitle
+from
+    Suppliers
+where ContactTitle != 'Marketing Manager'; -- MS SQL Server
+
+
+select
+    "SupplierID",
+    ContactName",
+    ContactTitle"
+from
+    "Suppliers"
+where "ContactTitle" != 'Marketing Manager'; -- PostgreSQL
+
+/*
+Learnings:
+Two ways to express "not": <>, !=
+*/
