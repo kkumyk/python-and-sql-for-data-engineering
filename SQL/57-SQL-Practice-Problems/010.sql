1b2be5dca43649e08915e4315f15e8ad03927904
kkumyk <k.kumykova@gmail.com>
2024-09-03 13:09:37 +0100

    add solution with order by

---
+++ SQL/57-SQL-Practice-Problems/010.sql
@@ -0,0 +1,5 @@
+-- 010. Employees, in order of age
+
+select FirstName, LastName, Title, BirthDate from Employees order by BirthDate; -- MS SQL Server
+
+select "FirstName", "LastName", "Title", "BirthDate" from "Employees" order by "BirthDate"; -- PostgreSQL
