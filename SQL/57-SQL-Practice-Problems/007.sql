3c0bf00bba1fe70d5d80d2ead574617c015c965f
kkumyk <k.kumykova@gmail.com>
2024-09-03 12:53:01 +0100

    add postgres solution

---
+++ SQL/57-SQL-Practice-Problems/007.sql
@@ -0,0 +1,20 @@
+-- 7. Products with "queso" in ProductName
+
+select
+    ProductID, ProductName
+from
+    Products
+where 
+    ProductName LIKE '%queso%'; -- MS SQL Server
+
+/*Learnings:
+
+The LIKE operator is always used with wildcards, such as % , which substitutes for any number of characters.
+
+Note that even though the search string used a lowercase "q" with the LIKE clause,
+the resulting rows will have an uppercase Q.
+*/
+
+select "ProductID", "ProductName"
+from "Products"
+where "ProductName" like '%queso%';
