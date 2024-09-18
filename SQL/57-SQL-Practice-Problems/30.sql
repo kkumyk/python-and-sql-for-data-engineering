1dc3d68ee3a2618a43781bf976f40a8e5ffb884f
kkumyk <k.kumykova@gmail.com>
2024-09-18 17:17:02 +0100

    inner and outer joins learning notes and examples

--- SQL/57-SQL-Practice-Problems/30.sql
+++ SQL/57-SQL-Practice-Problems/30.sql
@@ -34,4 +34,37 @@ RIGHT (OUTER) JOIN
 FULL (OUTER) JOIN
      - rows from both tables are kept, regardless of whether a matching row exists in the other table
 
+around_the_world_in_eighty_bottles=> select * from uk_white_grape_varieties;
+ id | grape_variety_name 
+----+--------------------
+  1 | Chardonnay
+  2 | Gewürztraminer
+  3 | Seyval Blanc
+  4 | Pinot Gris/Grigio
+  5 | Müller-Thurgau
+  6 | Madeleine Angevine
+  7 | Reichensteiner
+  8 | Schönburger
+  9 | Ortega
+(9 rows)
+
+around_the_world_in_eighty_bottles=> select * from china_white_grape_varieties;
+ id | grape_variety_name 
+----+--------------------
+  1 | Chardonnay
+  2 | Italian Riesling
+  3 | Longyan
+  4 | Vidal Blanc
+
 */
+
+--- INNER JOIN: find the white wine varieties that UK and China have in common
+
+SELECT china_white_grape_varieties.grape_variety_name as china_and_uk_common_white_grapes
+FROM china_white_grape_varieties
+INNER JOIN uk_white_grape_varieties
+    ON uk_white_grape_varieties.grape_variety_name = china_white_grape_varieties.grape_variety_name;
+
+--  china_and_uk_common_white_grapes 
+-- ----------------------------------
+--  Chardonnay
