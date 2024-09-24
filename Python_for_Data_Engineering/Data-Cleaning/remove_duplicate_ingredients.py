d6f63f2c3f0769dc0bbdf978f5bc3b2783710d36
kkumyk <k.kumykova@gmail.com>
2024-09-24 14:37:30 +0100

    adjust file paths

--- Python_for_Data_Engineering/Data-Cleaning/remove_duplicate_ingredients.py
+++ Python_for_Data_Engineering/Data-Cleaning/remove_duplicate_ingredients.py
@@ -5,7 +5,7 @@ Task: Identify unique values (ingredients) in the CSV file.
 import pandas as pd
 
 # read CSV file into Pandas DataFrame
-ingredient_pairings = pd.read_csv('ingredient_pairings.csv')
+ingredient_pairings = pd.read_csv('./data_inputs/ingredient_pairings.csv')
 
 # flaten the DataFrame into a list of all values
 all_ingredients = ingredient_pairings.values.flatten()
@@ -18,5 +18,5 @@ unique_ingredients_df = pd.DataFrame(unique_ingredients, columns=['Unique Ingred
 
 print(unique_ingredients_df.head(5))
 
-unique_ingredients_df.to_csv('unique_ingredients.csv', index=False)
+unique_ingredients_df.to_csv('./data_outputs/unique_ingredients.csv', index=False)
 
