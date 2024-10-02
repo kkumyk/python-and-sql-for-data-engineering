2dc5125bcda647b711eeea0e25214af9f9c63576
kkumyk <k.kumykova@gmail.com>
2024-10-02 14:16:11 +0100

    sql: use CAST to convert to DATE type

--- SQL/57-SQL-Practice-Problems/011.sql
+++ SQL/57-SQL-Practice-Problems/011.sql
@@ -1,24 +1,31 @@
-/* MS SQL Server */
-SELECT
-    FirstName
-    , LastName
-    , Title
-    , DataOnlyBirthData = convert(date, BirthDate)
-FROM Employees
-Order By BirthDate;
+/* 11. Show only the date with the date time field
 
+The original task is based on the assumption the the previous one returns time of the birthday field (00:00:00.000)/
+In output we see in Postgres however does not return the time part. As a result, we see the same output as in the task 010.sql:
 
-/* PostgreSQL
+ firstname | lastname  |          title           | dataonlybirthdata 
+-----------+-----------+--------------------------+-------------------
+ Margaret  | Peacock   | Sales Representative     | 1937-09-19
+ Nancy     | Davolio   | Sales Representative     | 1948-12-08
+ Andrew    | Fuller    | Vice President, Sales    | 1952-02-19
+
 CAST function converts one data type to another.
 CAST syntax: CAST(expression AS target_data_type) with expression to be value or column to be converted. 
 Use DATE keyword to convert the column to the DATE date type which only stores the date part (year, month, day).
 */
 
 SELECT
-    "FirstName",
-    "LastName",
-    "Title",
-    CAST("BirthDate" AS DATE) AS "DataOnlyBirthData" 
-    -- or use: "BirthDate"::DATE AS "DataOnlyBirthData"
-FROM "Employees"
-ORDER BY "BirthDate";
+    FirstName,
+    LastName,
+    Title,
+    CAST(BirthDate AS DATE) AS DataOnlyBirthData 
+FROM Employees
+ORDER BY BirthDate;
+
+SELECT
+    FirstName,
+    LastName,
+    Title,
+    BirthDate::DATE AS DataOnlyBirthData
+FROM Employees
+ORDER BY BirthDate;
