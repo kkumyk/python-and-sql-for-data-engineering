598f9889a1999b76977b1e69e2930388b9763762
kkumyk <k.kumykova@gmail.com>
2024-10-02 14:32:22 +0100

    sql: use concat

--- SQL/57-SQL-Practice-Problems/012.sql
+++ SQL/57-SQL-Practice-Problems/012.sql
@@ -1,5 +1,25 @@
--- 12. Employees full name
+/* 12. Employees full name
 
-select FirstName, LastName, FullName = FirstName + ' ' LastName from Employees; -- MS SQL Server
+There are benefits of using CONCAT over + or || especially when dealing with NULL values in data:
 
-select "FirstName", "LastName", concat("FirstName", ' ', "LastName") as "FullName" from "Employees";  -- PostgreSQL
+    - when you use || and one of the values is NULL, the result will be NULL:
+      SELECT 'Hello' || NULL || 'World';  # NULL
+
+    - CONCAT treats NULL values as empty string and only non-null values will be concatenated:
+      SELECT CONCAT('Hello', NULL, 'World'); # HelloWorld 
+
+
+ firstname | lastname  |     fullname     
+-----------+-----------+------------------
+ Nancy     | Davolio   | Nancy Davolio
+ Andrew    | Fuller    | Andrew Fuller
+ Janet     | Leverling | Janet Leverling
+ Margaret  | Peacock   | Margaret Peacock
+ Steven    | Buchanan  | Steven Buchanan
+ Michael   | Suyama    | Michael Suyama
+ Robert    | King      | Robert King
+ Laura     | Callahan  | Laura Callahan
+ Anne      | Dodsworth | Anne Dodsworth
+*/
+
+select FirstName, LastName, concat(FirstName, ' ', LastName) as FullName from Employees;
