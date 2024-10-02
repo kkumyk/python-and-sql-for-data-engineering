f43f0798b6461fa133a5e7db005e3701df39faf2
kkumyk <k.kumykova@gmail.com>
2024-10-02 13:48:06 +0100

    sql: order by sorts the result

--- SQL/57-SQL-Practice-Problems/010.sql
+++ SQL/57-SQL-Practice-Problems/010.sql
@@ -1,5 +1,15 @@
--- 010. Employees, in order of age
+/* 010. Employees, in order of age
 
-select FirstName, LastName, Title, BirthDate from Employees order by BirthDate; -- MS SQL Server
+For all employees return first name, last name, title, birth date and order by birth date so that the oldest will come up first.
 
-select "FirstName", "LastName", "Title", "BirthDate" from "Employees" order by "BirthDate"; -- PostgreSQL
+Order By clause is used to sort the results. 
+
+ firstname | lastname  |          title           | birthdate  
+-----------+-----------+--------------------------+------------
+ Margaret  | Peacock   | Sales Representative     | 1937-09-19
+ Nancy     | Davolio   | Sales Representative     | 1948-12-08
+ Andrew    | Fuller    | Vice President, Sales    | 1952-02-19
+
+*/
+
+select FirstName, LastName, Title, BirthDate from Employees order by BirthDate;
