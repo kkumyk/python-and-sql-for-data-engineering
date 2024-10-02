04b184c1da741ff8e15ef05b2229fffa8baf06d4
kkumyk <k.kumykova@gmail.com>
2024-10-02 13:42:40 +0100

    sql: use In operator to find all orders from Latin America

--- SQL/57-SQL-Practice-Problems/009.sql
+++ SQL/57-SQL-Practice-Problems/009.sql
@@ -1,5 +1,21 @@
 -- 9. Orders shipping to any country in Latin America
 
-select OrderID, CustomerID, ShipCountry from Orders where ShipCountry in ('Brazil', 'Argentina', 'Venezuela') -- MS SQL Server
+select OrderID, CustomerID, ShipCountry from Orders where ShipCountry in ('Brazil', 'Argentina', 'Venezuela');
 
-select "OrderID", "CustomerID", "ShipCountry" from "Orders" where "ShipCountry" in ('Brazil', 'Argentina', 'Venezuela')  -- PostgreSQL
+/*
+
+Using In operator to find orders from Latin America.  
+
+Output Result:
+
+ orderid | customerid | shipcountry 
+---------+------------+-------------
+   10250 | HANAR      | Brazil
+   10253 | HANAR      | Brazil
+   10256 | WELLI      | Brazil
+   10257 | HILAA      | Venezuela
+   10261 | QUEDE      | Brazil
+   10268 | GROSR      | Venezuela
+   10283 | LILAS      | Venezuela
+   10287 | RICAR      | Brazil ...
+*/
