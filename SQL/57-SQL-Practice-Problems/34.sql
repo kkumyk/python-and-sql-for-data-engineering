8aa10055a7a0f3e47ec65669396cc8ed174e35d6
kkumyk <k.kumykova@gmail.com>
2024-10-10 15:21:03 +0100

    sql: update query WIP

--- SQL/57-SQL-Practice-Problems/34.sql
+++ SQL/57-SQL-Practice-Problems/34.sql
@@ -1,71 +1,26 @@
-/* 32. High-value customers - with discount
-
-- now high-value customers are:
-    - customers which have made orders totaling more than 112000 in 2056 taking into consideration the discount
+/* 34. High-value customers - with discount
+    - use discount when calculation high-value customers
     - the discount is applied as a percentage
-    - use parenthesis around (1 - Discount) to make sure that calculation is done first
-
-customers:
-    customer_id |  customer_name   
-    -------------+------------------
-            1 | Drunken Botanist
-            2 | Drunken Chemist
-            3 | Sober Botanist
-            4 | Sober Chemist
-
-orders:
- order_id | order_date | customer_id 
-----------+------------+-------------
-        1 | 2055-12-31 |           1
-        2 | 2053-02-23 |           2
-        3 | 2055-10-11 |           3
-        4 | 2053-02-22 |           4
-        5 | 2056-10-11 |           1
-        7 | 2056-05-12 |           2
-        8 | 2056-05-12 |           3
-       10 | 2056-10-11 |           1
-       11 | 2056-02-22 |           2
-
-order_details:
- order_id | product_id | unit_price | quantity | discount 
-----------+------------+------------+----------+----------
-        4 |          2 |     999.99 |        1 |         
-        8 |          2 |     999.99 |        1 |         
-       10 |          2 |     999.99 |       10 |         
-       11 |          2 |     999.99 |       11 |         
-        1 |          1 |    3000.77 |      666 |     0.15
-        2 |          2 |     999.99 |      999 |     0.15
-        3 |          2 |     999.99 |        1 |     0.05
-        5 |          2 |     999.99 |      144 |     0.07
-        7 |          2 |     999.99 |      122 |     0.09
+    - use parenthesis around (1 - Discount) to make sure that calculation is done first.
 */
 
--- PostgreSQL
-
 select
-    customers.customer_id,
-    customers.customer_name,
-    sum(quantity * unit_price) as totals_without_discount,
-    sum(quantity * unit_price * (1-discount)) as totals_with_discount
+    customers.customerid,
+    customers.contactname,
+    sum(quantity * unitprice) as totals_without_discount,
+    sum(quantity * unitprice * (1-discount)) as totals_with_discount
 from customers
     join orders
-        on orders.customer_id = customers.customer_id
+        on orders.customerid = customers.customerid
     join order_details
-        on orders.order_id = order_details.order_id
+        on orders.orderid = order_details.orderid
 where
-    order_date >= '2056-01-01' and order_date < '2057-01-01'
+    orderdate >= '2016-01-01' and orderdate < '2017-01-01'
 group by
-    customers.customer_id,
-    customers.customer_name
-having sum(quantity * unit_price * (1-discount)) > 112000
+    customers.customerid,
+    customers.contactname
+having sum(quantity * unitprice * (1-discount)) > 15000
 order by totals_with_discount desc;
-
-
--- Query Result:
-
- customer_id |  customer_name   | totals_without_discount | totals_with_discount 
--------------+------------------+-------------------------+----------------------
-           1 | Drunken Botanist |               153998.46 |          133918.6608
 
 /*
 Learnings:
