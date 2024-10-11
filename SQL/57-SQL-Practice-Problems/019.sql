1df695fbd4aba1be585356d2afb5be2cbfcc1a94
kkumyk <k.kumykova@gmail.com>
2024-10-11 12:06:23 +0100

    sql: update postgres query and add expected result

--- SQL/57-SQL-Practice-Problems/019.sql
+++ SQL/57-SQL-Practice-Problems/019.sql
@@ -1,22 +1,33 @@
--- 19. Orders and the Shipper that was used
-
-Select
-    OrderID, OrderDate = convert(date, OrderDate), Shipper = CompanyName
-From Orders
-    Join Shippers
-        on Shippers.ShipperID = Orders.ShipVia
-Where ProductID < 10270
-Order by OrderID -- MS SQL Server
-
-SELECT OrderID, OrderDate::date AS OrderDate, CompanyName AS Shipper
-FROM Orders
-JOIN Shippers ON Shippers.ShipperID = Orders.ShipVia
-WHERE ProductID < 10270
-ORDER BY OrderID; -- PostgreSQL
-
 /*
-Learnings:
+19. Orders and the Shipper that was used
 
-Casting Data Types
-In Postgres, ::type casts columns to different data types; cast a column to a date using ::date.
+Orders:
+ orderid | customerid | employeeid | orderdate  | requireddate | shippeddate | shipvia | freight |         shipname          |    shipaddress     |    shipcity    | shipregion | shippostalcode | shipcountry 
+---------+------------+------------+------------+--------------+-------------+---------+---------+---------------------------+--------------------+----------------+------------+----------------+-------------
+   10248 | VINET      |          5 | 1996-07-04 | 1996-08-01   | 1996-07-16  |       3 |   32.38 | Vins et alcools Chevalier | 59 rue de l'Abbaye | Reims          |            | 51100          | France
+   10249 | TOMSP      |          6 | 1996-07-05 | 1996-08-16   | 1996-07-10  |       1 |   11.61 | Toms Spezialitäten        | Luisenstr. 48      | Münster        |            | 44087          | Germany
+   10250 | HANAR      |          4 | 1996-07-08 | 1996-08-05   | 1996-07-12  |       2 |   65.83 | Hanari Carnes             | Rua do Paço, 67    | Rio de Janeiro | RJ         | 05454-876      | Brazil
+
+Shippers:
+ shipperid |   companyname    |     phone      
+-----------+------------------+----------------
+         1 | Speedy Express   | (503) 555-9831
+         2 | United Package   | (503) 555-3199
+         3 | Federal Shipping | (503) 555-9931
+
+Expected Result:
+ orderid | orderdate  |     shipper      
+---------+------------+------------------
+   10248 | 1996-07-04 | Federal Shipping
+   10249 | 1996-07-05 | Speedy Express
+   ...
+   10268 | 1996-07-30 | Federal Shipping
+   10269 | 1996-07-31 | Speedy Express
+(22 rows)
 */
+
+SELECT orderid, orderdate, companyname AS shipper
+FROM orders o
+JOIN shippers s ON s.ShipperID = o.shipvia
+WHERE orderid < 10270
+ORDER BY orderid;
