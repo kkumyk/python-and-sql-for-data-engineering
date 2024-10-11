bf46c76ca91979bc68cf0fb7044edb96099f0725
kkumyk <k.kumykova@gmail.com>
2024-10-11 11:11:56 +0100

    sql: update Postgres query and add learning comments on use of aggregate functions

--- SQL/57-SQL-Practice-Problems/015.sql
+++ SQL/57-SQL-Practice-Problems/015.sql
@@ -1,7 +1,24 @@
--- 15. When was the first order?
+/*
+15. When was the first order?
 
-SELECT FirstOrder = min(OrderDate)
-FROM Orders; -- MS SQL Server
+Show the date of the first order ever made in the orders table:
+ orderid | customerid | employeeid | orderdate  | requireddate | shippeddate | shipvia | freight |         shipname          |    shipaddress     |    shipcity    | shipregion | shippostalcode | shipcountry 
+---------+------------+------------+------------+--------------+-------------+---------+---------+---------------------------+--------------------+----------------+------------+----------------+-------------
+   10248 | VINET      |          5 | 1996-07-04 | 1996-08-01   | 1996-07-16  |       3 |   32.38 | Vins et alcools Chevalier | 59 rue de l'Abbaye | Reims          |            | 51100          | France
+   10249 | TOMSP      |          6 | 1996-07-05 | 1996-08-16   | 1996-07-10  |       1 |   11.61 | Toms Spezialitäten        | Luisenstr. 48      | Münster        |            | 44087          | Germany
+   10250 | HANAR      |          4 | 1996-07-08 | 1996-08-05   | 1996-07-12  |       2 |   65.83 | Hanari Carnes             | Rua do Paço, 67    | Rio de Janeiro | RJ         | 05454-876      | Brazil
 
-SELECT min(OrderDate) as FirstOrder
-FROM Orders; -- PostgreSQL
+Expected results:
+ First Order 
+-------------
+ 1996-07-04
+*/
+
+select min(orderdate) as "First Order" from orders;
+
+/*
+Learnings:
+
+For the aggregate function Count, you don't need to specify a column name - just count(*) will work.
+For other aggregate functions such as Min, Avg, Sum etc., you will need to specify a column name since you are not just counting all rows.
+*/
