c273d1028018d64641989bb2ee54caaf496d74e6
kkumyk <k.kumykova@gmail.com>
2024-10-15 16:46:33 +0100

    sql: add context to the the total products in each category task

--- SQL/57-SQL-Practice-Problems/020.sql
+++ SQL/57-SQL-Practice-Problems/020.sql
@@ -1,14 +1,39 @@
--- 20. Categories, and the total products in each category
+/* 20. Categories, and the total products in each category
+- return total number of products in each category
+- sort the result by total number of products in descending order
+- combine join and group by
 
-Select CompanyName, TotalPRoducts = count(*)
-From Products 
-    Join Categories on Products.CategoryID = Categories.CategoryID    
-Group by CompanyName
-Order by count(*) desc-- MS SQL Server
+Products:
+ productid |  productname  | supplierid | categoryid |   quantityperunit   | unitprice | unitsinstock | unitsonorder | reorderlevel | discontinued 
+-----------+---------------+------------+------------+---------------------+-----------+--------------+--------------+--------------+--------------
+         1 | Chai          |          8 |          1 | 10 boxes x 30 bags  |        18 |           39 |            0 |           10 |            1
+         2 | Chang         |          1 |          1 | 24 - 12 oz bottles  |        19 |           17 |           40 |           25 |            1
+         3 | Aniseed Syrup |          1 |          2 | 12 - 550 ml bottles |        10 |           13 |           70 |           25 |            0 ...
 
+Categories:
+ categoryid | categoryname |                        description                         | picture 
+------------+--------------+------------------------------------------------------------+---------
+          1 | Beverages    | Soft drinks, coffees, teas, beers, and ales                | \x
+          2 | Condiments   | Sweet and savory sauces, relishes, spreads, and seasonings | \x
+          3 | Confections  | Desserts, candies, and sweet breads                        | \x ...
 
-SELECT CompanyName, COUNT(*) AS TotalProducts
-FROM Products
-JOIN Categories ON Products.CategoryID = Categories.CategoryID
-GROUP BY CompanyName
-ORDER BY COUNT(*) DESC; -- PostgreSQL
+Expected Result:
+  categoryname  | totalproducts 
+----------------+---------------
+ Confections    |            13
+ Beverages      |            12
+ Seafood        |            12
+ Condiments     |            12
+ Dairy Products |            10
+ Grains/Cereals |             7
+ Meat/Poultry   |             6
+ Produce        |             5
+
+Task's Focus: A parent-child relationship between two joined tables.
+*/
+
+SELECT categoryname, COUNT(productid) as TotalProducts
+FROM products 
+JOIN categories ON categories.categoryid=products.categoryid  
+GROUP BY categoryname
+ORDER BY COUNT(productid) DESC;
