559f173de3977bb6826f16d03fe416c85fc4cf49
kkumyk <k.kumykova@gmail.com>
2024-10-15 16:47:01 +0100

    sql: add context to the Total customers per country/city task

--- SQL/57-SQL-Practice-Problems/021.sql
+++ SQL/57-SQL-Practice-Problems/021.sql
@@ -1,20 +1,32 @@
--- 21. Total customers per country/city
+/* 21. Total customers per country/city
+- show the total number of customers per country and city
+- hint: as you can have multiple fields in a select clause, you can also have multiple fields ina group by clause
 
--- MS SQL Server
-Select Country, City, TotalCustomers = count(*)
-From Customers
-Group by Country, City
-Order by count(*) desc
+Customers:
+ customerid |            companyname             |  contactname   |     contacttitle     |            address            |    city     | region | postalcode | country |    phone     |     fax      
+------------+------------------------------------+----------------+----------------------+-------------------------------+-------------+--------+------------+---------+--------------+--------------
+ ALFKI      | Alfreds Futterkiste                | Maria Anders   | Sales Representative | Obere Str. 57                 | Berlin      |        | 12209      | Germany | 030-0074321  | 030-0076545
+ ANATR      | Ana Trujillo Emparedados y helados | Ana Trujillo   | Owner                | Avda. de la Constitución 2222 | México D.F. |        | 05021      | Mexico  | (5) 555-4729 | (5) 555-3745
+ ANTON      | Antonio Moreno Taquería            | Antonio Moreno | Owner                | Mataderos  2312               | México D.F. |        | 05023      | Mexico  | (5) 555-3932 | 
 
--- PostgreSQL
-SELECT Country, City, COUNT(*) AS TotalCustomers
-FROM Customers
-GROUP BY Country, City
-ORDER BY COUNT(*) DESC;
+Expected Results:
+   country   |      city       | totalcustomers 
+-------------+-----------------+----------------
+ UK          | London          |              6
+ Mexico      | México D.F.     |              5
+ Brazil      | Sao Paulo       |              4
+ Brazil      | Rio de Janeiro  |              3 ...
+
+ Task's Focus: group by  multiple fields
+*/
+
+select country, city, count(*) as TotalCustomers
+from customers
+group by country, city
+order by count(*) DESC;
 
 /*
 Learnings:
-
 Once you have a Group by clause in a SQL statement, every field that appears in the Select statement needs
 to either appear in the Group by clause, or needs to have some kind of aggregate function applied to it.
 */
