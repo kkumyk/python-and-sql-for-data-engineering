f7ef9c688d53bc260fbf064f261f790d35156fdf
kkumyk <k.kumykova@gmail.com>
2024-10-16 18:35:20 +0100

    sql: rewrite query with alias for the (inner) joins across multiple tables task

--- SQL/57-SQL-Practice-Problems/029.sql
+++ SQL/57-SQL-Practice-Problems/029.sql
@@ -1,18 +1,30 @@
--- 29. Employee/Order detail report (join four tables)
+/*
+29. Employee/Order detail report (join four tables) displaying only specific fields
 
--- MS SQL Server
+Expected Result:
+ employeeid | lastname  | orderid |           productname            | quantity 
+------------+-----------+---------+----------------------------------+----------
+          5 | Buchanan  |   10248 | Queso Cabrales                   |       12
+          5 | Buchanan  |   10248 | Singaporean Hokkien Fried Mee    |       10
+          5 | Buchanan  |   10248 | Mozzarella di Giovanni           |        5
+          6 | Suyama    |   10249 | Tofu                             |        9
+          6 | Suyama    |   10249 | Manjimup Dried Apples            |       40 ... 
+
+Focus: practice (inner) joins across multiple tables */
+
 SELECT
-    Employees.EmployeeID, 
-    Employees.LastName,
-    Orders.OrderID,
-    Products.ProductName,
-    OrderDetails.Quantity
-FROM Employees
-    JOIN Orders
-        ON Orders.EmployeeID = Employees.EmployeeID
-    JOIN OrderDetails
-        ON Order.OrderID = OrderDetails.OrderID
-    JOIN Products
-        ON Products.ProductID = OrderDetails.ProductID
+  o.employeeid,
+  e.lastname,
+  o.orderid,
+  p.productname,
+  od.quantity
+FROM
+  orders o
+JOIN
+  employees e on o.employeeid = e.employeeid
+JOIN
+  order_details od on o.orderid = od.orderid
+JOIN
+  products p on od.productid = p.productid
 ORDER BY
-    Orders.OrderID, Products.ProductID
+  o.orderid, od.productid;
