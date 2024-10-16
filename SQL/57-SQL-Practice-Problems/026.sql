aae2b7a19b72dd9057e34e7b5674a0263b52f18a
kkumyk <k.kumykova@gmail.com>
2024-10-16 16:53:26 +0100

    sql: get high freight charges in a specific year using built-in functions

--- SQL/57-SQL-Practice-Problems/026.sql
+++ SQL/57-SQL-Practice-Problems/026.sql
@@ -1,40 +1,39 @@
--- 026. High freight charges in 2015
+/* 026. High freight charges in 2015
+- what are the high freight charges for orders in 2015
+ orderid | customerid | employeeid | orderdate  | requireddate | shippeddate | shipvia | freight |         shipname          |    shipaddress     |    shipcity    | shipregion | shippostalcode | shipcountry 
+---------+------------+------------+------------+--------------+-------------+---------+---------+---------------------------+--------------------+----------------+------------+----------------+-------------
+   10248 | VINET      |          5 | 1996-07-04 | 1996-08-01   | 1996-07-16  |       3 |   32.38 | Vins et alcools Chevalier | 59 rue de l'Abbaye | Reims          |            | 51100          | France
+   10249 | TOMSP      |          6 | 1996-07-05 | 1996-08-16   | 1996-07-10  |       1 |   11.61 | Toms Spezialitäten        | Luisenstr. 48      | Münster        |            | 44087          | Germany
+   10250 | HANAR      |          4 | 1996-07-08 | 1996-08-05   | 1996-07-12  |       2 |   65.83 | Hanari Carnes             | Rua do Paço, 67    | Rio de Janeiro | RJ         | 05454-876      | Brazil
 
--- MS SQL Server
-Select Top 3
-    ShipCountry,
-    AverageFright = Avg(freight)
-From Orders
-Where OrderDate >= '20150101' and OrderDate < '20160101' -- YYYYMMDD
-Group By ShipCountry
-Order By AverageFright desc
+Expected Result:
+ shipcountry |    avg_freight     
+-------------+--------------------
+ Austria     | 178.36428569612048
+ Switzerland | 117.17749977111816
+ Sweden      |  105.1599993986242
+(3 rows)
 
-/*
-Learnings:
-Where year(OrderData) = 2015
-*/
-
--- PostgreSQL
-SELECT
-    ShipCountry,
-    AVG(freight) AS AverageFreight
-FROM Orders
-WHERE OrderDate >= '2015-01-01'
-  AND OrderDate < '2016-01-01'
-GROUP BY ShipCountry
-ORDER BY AverageFreight DESC
-LIMIT 3;
-
+Focus: Use of built-in functions such as EXTRACT and DATE_PART */
 
 SELECT
-    ShipCountry,
-    AVG(freight) AS AverageFreight
-FROM Orders
-WHERE EXTRACT(YEAR FROM OrderDate) = 2015
-GROUP BY ShipCountry
-ORDER BY AverageFreight DESC
+    shipcountry,
+    AVG(freight) AS avg_freight
+FROM orders
+WHERE EXTRACT(YEAR FROM orderdate) = 1997
+GROUP BY shipcountry
+ORDER BY avg_freight DESC
 LIMIT 3;
 
+SELECT
+  shipcountry,
+  avg(freight) as avg_freight
+FROM orders
+WHERE date_part('year', orderdate) = 1997
+GROUP BY shipcountry
+ORDER BY avg_freight DESC
+LIMIT 3;
+  
 /*
 Learnings:
 The date format in PostgreSQL is YYYY-MM-DD.
