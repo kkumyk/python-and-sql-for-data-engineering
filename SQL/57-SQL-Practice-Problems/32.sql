9a9776a84e17a9615dca22f0104ba6a8c072b272
kkumyk <k.kumykova@gmail.com>
2024-10-17 16:42:43 +0100

    sql: rewrite query for High-value customers task

--- SQL/57-SQL-Practice-Problems/32.sql
+++ SQL/57-SQL-Practice-Problems/32.sql
@@ -1,74 +1,43 @@
 /* 32. High-value customers
 
-- find high-value customers with order value at least 999.99
+- find high-value customers with order value at least 10000
 - high-value customers are:
-    - made at least 1 order equal over 999.99
-    - for orders made only in 2056
+    - made at least 1 order equal over 10000 or more not including the discount
+    - for orders made only in 1998
 
-customers:
-    customer_id |  customer_name   
-    -------------+------------------
-            1 | Drunken Botanist
-            2 | Drunken Chemist
-            3 | Sober Botanist
-            4 | Sober Chemist
+Expected Result:
+ customerid |         companyname          | orderid | total_amount 
+------------+------------------------------+---------+--------------
+ QUICK      | QUICK-Stop                   |   10865 |     17250.00
+ SAVEA      | Save-a-lot Markets           |   11030 |     16321.90
+ HANAR      | Hanari Carnes                |   10981 |     15810.00
+ KOENE      | Königlich Essen              |   10817 |     11490.70
+ RATTC      | Rattlesnake Canyon Grocery   |   10889 |     11380.00
+ HUNGO      | Hungry Owl All-Night Grocers |   10897 |     10835.24
 
-orders:
- order_id | order_date | customer_id 
-----------+------------+-------------
-        1 | 2055-12-31 |           1
-        2 | 2053-02-23 |           2
-        3 | 2055-10-11 |           3
-        4 | 2053-02-22 |           4
-        5 | 2056-10-11 |           1
-        7 | 2056-05-12 |           2
-        8 | 2056-05-12 |           3
 
-order_details:
- order_id | product_id | unit_price | quantity 
-----------+------------+------------+----------
-        1 |          1 |    3000.77 |      666
-        2 |          2 |     999.99 |      999
-        3 |          2 |     999.99 |        1
-        4 |          2 |     999.99 |        1
-        5 |          2 |     999.99 |      144
-        7 |          2 |     999.99 |      122
-        8 |          2 |     999.99 |        1
+Learnings: Aggregate functions can only be used to filter in the Having clause, not the Where clause.
 
+Focus: aggregate functions in the HAVING clause
 */
 
--- PostgreSQL
+SELECT
+  c.customerid,
+  c.companyname,
+  o.orderid,
+  round(sum(od.unitprice * od.quantity)::numeric, 2) as total_amount
+FROM
+  customers c
+JOIN
+  orders o on c.customerid = o.customerid
+JOIN
+  order_details od on o.orderid = od.orderid
+WHERE
+  date_part('year', o.orderdate) = 1998
+GROUP BY
+  c.customerid, c.companyname, o.orderid
+HAVING
+  sum(od.unitprice * od.quantity) >= 10000
+ORDER BY
+  total_amount DESC;
 
-select
-    customers.customer_id,
-    customers.customer_name,
-    orders.order_id,
-    sum(quantity * unit_price) as total_order_amount
-from customers
-    join orders
-        on orders.customer_id = customers.customer_id
-    join order_details
-        on orders.order_id = order_details.order_id
-where
-    order_date >= '2056-01-01' and order_date < '2057-01-01'
-group by
-    customers.customer_id,
-    customers.customer_name,
-    orders.order_id
-having sum(quantity * unit_price) > 999.99
-order by total_order_amount desc;
-
--- Query Result:
-
---  customer_id |  customer_name   | order_id | total_order_amount 
--- -------------+------------------+----------+--------------------
---            1 | Drunken Botanist |        5 |          143998.56
---            2 | Drunken Chemist  |        7 |          121998.78
-
-
-/*
-Learnings:
-
-Aggregate functions can only be used to filter in the Having clause, not the Where clause.
-
-*/
